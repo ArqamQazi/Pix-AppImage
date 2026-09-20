3a4b5fceaa1af2601f79639cd26c369d9b3a5023
ArqamQazi <arqamqazi549@gmail.com>
2026-09-20 11:25:20 +0530

    Pix init

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
-
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q maui-pix 2>/dev/null | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DESKTOP=/usr/share/applications/org.kde.pix.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/pix.svg
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/pix
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+# Test the AppImage
+quick-sharun --simple-test ./dist/*.AppImage
